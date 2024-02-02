c8450180394269e1bf352dbc9dc15cc0413953a7
Solrac <solrac@Carloss-MacBook-Pro.local>
2024-02-01 22:06:14 -0500

    buid config

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,23 @@
-console.log("this is a test")
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+const port = process.env.PORT || 1234;
+
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+    const indexPath = process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production'
+        ? join(__dirname, 'index.html')
+        : join(__dirname, '..', '..', 'index.html');
+
+    console.log(indexPath, 'This is indexpathladlksjdka')
+    req.body
+    res.sendFile(indexPath);
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
